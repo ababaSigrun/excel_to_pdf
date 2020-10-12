1175b1f57638f1c25f08e73083bd1833e59e3e4d
ababaSigrun <ababa715git@gmail.com>
2020-10-12 02:35:37 +0000

    削除部分をメソッド化

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import os
 import glob
 
 
-#  1.まずは出力先の中身を空にする。
-for file in glob.glob('./OutputPDF/*.pdf'):
-    os.remove(file)
+
+
+#  出力先の中身を空にする。
+def delPDF ():
+    for file in glob.glob('./OutputPDF/*.pdf'):
+        os.remove(file)
 
 # 該当のUserIdが表記されている箇所をメモ帳から取得する。
 
 
-# 
+#
+# メインメソッド
+
+#出力先の削除を行う。
+delPDF()
